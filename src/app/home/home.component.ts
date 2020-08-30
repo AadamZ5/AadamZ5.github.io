fdfdc749b95a9b4e829a4562d7cc7a3a54eb6a62
Aadam Zocolo <aadamz5@AZ87up.localdomain>
2020-08-30 18:30:28 -0400

    More progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from "@angular/animations";
+import { Router } from '@angular/router';
 
 interface Action{
   label?: string,
   icon?: string,
   callback?: (event: any) => any;
   style?: "raised" | "flat" | "stroked" | "icon" | "fab" | "minifab" | "basic";
-  color?: "primary" | "secondary" | "warn";
+  color?: "primary" | "accent" | "warn";
 }
 
 interface CardItem{
@@ -15,56 +17,93 @@ interface CardItem{
   actions?: Action[];
 }
 
+const card_trigger = trigger("card-trigger", [
+  state('*', style({
+    visibility: "hidden",
+    opacity: 0, 
+    transform: "translateY(-100px)",
+  })),
+  state('hidden', style({
+    visibility: "hidden",
+    opacity: 0, 
+    transform: "translateY(-100px)",
+  })),
+  state('shown', style({
+    visibility: "visible",
+    opacity: 1,
+    transform: "none",
+  })),
+  transition('hidden <=> shown', [
+    animate('.5s ease-out'),
+  ]),
+  transition('* <=> shown', [
+    animate('.5s ease-out', style(
+      {
+        visibility: "visible",
+        opacity: 1,
+        transform: "none",
+      }
+    )),
+  ])
+]);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    card_trigger,
+  ]
 })
 export class HomeComponent implements OnInit {
 
+  show_cards: boolean = false;
+  
   items: CardItem[] = [
     {
-      title: "The long text",
-      text: "This is some test text! Really really long test text so we can see how this stuff will look on the screen, and then make adjustments as we need to perfect the visual look and design of the website.",
-      actions: [
-        {
-          callback: (e) => {
-            console.log("They clicked!");
-          },
-          label: "Sweet!",
-          style: "stroked"
-        },
-        {
-          callback: (e) => {
-            console.warn("Warning!");
-          },
-          label: "Warning",
-          color: "warn",
-          style: "stroked"
-        }
-      ],
-      image_src: "https://cdn.custom-cursor.com/packs/2344/pack2969.png",
+      title: "Pride",
+      text: `Being prideful of the work being done is more important than you may think. A sense of pride in one's work will inherently cause motivation. A lack of pride can cause a fallout in percieved purpose or motivation. 
+      
+      Additionally, the work being done should be well enough that one can be prideful of it. Too many times I've seen projects that people do not have a strong will or desire to work on. They have a lack of pride. The project is failing, solutions are overlooked, and insensible actions are taken for various problems.`
     },
     {
-      title: "Lorem Ipsum?!",
-      text: "Jesus christ! Who the hell even speaks latin anymore? That language is dead! How dare you provoke me and my fellow anti-latin-language friends! You should find your place, which is far from here! Latin will never ever overcome English! Our president may be orange... well, I've got nothing good to say about him either.",
-      image_src: "https://external-preview.redd.it/34qKOC48JJMsf92Rs9_cobDnVYz_sl87CElrhVG1Hus.png?auto=webp&s=f3811a4ba5aa7601e7a5a5310737c15556129a26",
+      title: "Efficiency",
+      text: `Efficiency is important. If there is a more elegant, more meaningful way to get something done, it should be done.`
     },
     {
-      title: "Hey!",
-      text: "Hello!",
-      image_src: "https://s3.thingpic.com/images/Mt/UX9NhUtAxs9V9CmhtggcMZFB.jpeg",
+      title: "Integrity",
+      text: `Integrity is important. Holding true to a certain requirement or goal is important. This goes both ways between a designer, engineer, or programmer, and the manager. I strive to keep integrity high for projects, whatever the scope. A lack in integrity will certainly be appearant in the end-product, and ultimately hurt both the user and the maker of the product. I strive to keep my projects reliable, honest, and to the point. No web-app or back-end solution should be rushed to work, cut corners, or fake in functionality. This requires understanding from both parties.`,
+      //image_src: "https://www.rootsofaction.com/wp-content/uploads/2017/02/Integrity-borders.jpg",
+    },
+    {
+      title: "Creativeness",
+      text: "Creativeness leads us to unexpected solutions. Creativeness can solve issues we believe are unsolvable. Thinking outside the box isn't something that can be taught, it must be understood inherently. "
+    },
+    {
+      title: "Polish",
+      text: `Designs need refined again and again, until you see little improvement from one iteration to the next. Polish is important for both internal products, and products going out into the hands of people. 
+      
+      A design without polish can leave users in a jarring experience, which will break flow. When things are polished and work well together, the flow is almost euphoric. Things feel like the future when they are polished, and thats the feeling I want people to have when they use my projects, designs, or products.`
+    },
+    {
+      title: "Get to know me",
+      text: `Why don't you get to know me a little better? Lets go to the 'About' page.`,
       actions: [
         {
-          color: "primary",
-          label: "Contact 'them'",
-          style: "raised",
+          callback: () => {
+            this.router.navigate(['about']);
+          },
+          color: "accent",
+          label: "Let's go",
+          style: "stroked",
         }
       ]
-    },
+    }
   ];
 
-  constructor() { }
+  constructor(private router: Router) {
+    
+  }
 
   ngOnInit(): void {
   }
