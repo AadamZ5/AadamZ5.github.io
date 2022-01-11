8a60e54bddc34a25abc86870984f2e1a8241b2e8
Aadam Zocolo <azocolo@gmail.com>
2022-01-10 20:29:45 -0500

    Fix up some errors and improve sidenav-hook

--- src/app/sidenav-hook.ts
+++ src/app/sidenav-hook.ts
@@ -1,12 +1,32 @@
 import { Provider } from "@angular/core";
 import { MatSidenav } from "@angular/material/sidenav"
-import { BehaviorSubject, Observable } from "rxjs"
+import { BehaviorSubject, Observable, Subscription } from "rxjs"
 
 export class SidenavHook {
 
+    private _sidenavOpen$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+    get sidenavOpen$(): Observable<boolean> {
+        return this._sidenavOpen$.asObservable();
+    }
+
     private _sidenavInstance: BehaviorSubject<MatSidenav | null> = new BehaviorSubject<MatSidenav | null>(null);
-    get sidenavInstance(): Observable<MatSidenav | null> {
+    get sidenavInstance$(): Observable<MatSidenav | null> {
         return this._sidenavInstance.asObservable();
+    }
+
+    constructor() {
+        let sidenavSubscription: Subscription | undefined;
+        this.sidenavInstance$.subscribe((sidenav) => {
+            sidenavSubscription?.unsubscribe();
+            sidenavSubscription = undefined;
+            if (sidenav) {
+                sidenavSubscription = sidenav.openedChange.subscribe((open) => {
+                    this._sidenavOpen$.next(open);
+                });
+            } else {
+                this._sidenavOpen$.next(false);
+            }
+        })
     }
 
     hookSidenav(sidenavInstance: MatSidenav | null) {
